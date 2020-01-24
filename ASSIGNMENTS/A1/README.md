d1b3e29c5d1268d50aba89fa362458054c4d951f
gbrich <gabriella.rich@yahoo.com>
2020-01-24 11:42:34 -0700

    A1

---
+++ ASSIGNMENTS/A1/README.md
@@ -0,0 +1,23 @@
+# Assignment 1
+`Due Jan 24, before midnight`
+
+## Task
+
+Please complete the `mathServer.js`. As an example this works
+
+http://localhost:1234/add?a=1&b=4
+
+You need to make the following request handlers:
+
+http://localhost:1234/sub?a=1&b=4
+
+http://localhost:1234/mul?a=1&b=4
+
+http://localhost:1234/div?a=1&b=4
+
+
+## Usage (handout)
+
+$ `node mathServer.js`
+
+got to <http://127.0.0.1:1234> in your browser.
---
+++ ASSIGNMENTS/A1/mathServer.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+var hostname = process.env.HOSTNAME || 'localhost';
+var port = 1234;
+
+app.get("/add", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    res.send((a+b).toString()); // send response body
+});
+app.get("/subtract", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    res.send((a-b).toString()); // send response body
+});
+app.get("/multiply", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    res.send((a*b).toString()); // send response body
+});
+app.get("/divide", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    res.send((a/b).toString()); // send response body
+});
+
+app.use(methodOverride());
+app.use(bodyParser());
+app.use(errorHandler());
+
+console.log("Simple static server listening at http://" + hostname + ":" + port);
+app.listen(port);
